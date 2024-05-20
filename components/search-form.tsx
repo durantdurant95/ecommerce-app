7bc182a526a234db23442f42aaa08b6663bb0d9b
Alejandro Pérez Durán <aperez@datamart.cl>
2024-05-20 10:00:39 -0400

    Add Suspense with Skeleton to the Search Form

--- components/search-form.tsx
+++ components/search-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
+import { Skeleton } from "./ui/skeleton";
 
 export default function SearchForm() {
   const searchParams = useSearchParams();
@@ -10,13 +12,15 @@ export default function SearchForm() {
       action="/products/search"
       className="hidden gap-4 tracking-normal lg:flex"
     >
-      <Input
-        className="w-80"
-        type="text"
-        name="product"
-        placeholder="Search for cool stuff!"
-        defaultValue={searchParams.get("product") ?? ""}
-      />
+      <Suspense fallback={<Skeleton className="h-10 w-80" />}>
+        <Input
+          className="w-80"
+          type="text"
+          name="product"
+          placeholder="Search for cool stuff!"
+          defaultValue={searchParams.get("product") ?? ""}
+        />
+      </Suspense>
       <Button type="submit">Search</Button>
     </form>
   );
