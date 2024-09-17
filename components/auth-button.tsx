d159a7ea32f4df05f4c5af847e539be8013ef8dc
Alejandro Pérez Durán <aperez@datamart.cl>
2024-09-16 22:29:56 -0400

    Add pages for sign in and sign up

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,6 +1,5 @@
 import { createClient } from "@/utils/supabase/server";
 import { LogOut, Settings, User } from "lucide-react";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import { Avatar, AvatarFallback } from "./ui/avatar";
 import {
@@ -14,7 +13,6 @@ import {
 
 export default async function AuthButton() {
   const supabase = createClient();
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
@@ -24,14 +22,14 @@ export default async function AuthButton() {
 
     const supabase = createClient();
     await supabase.auth.signOut();
-    return redirect("/login");
+    return redirect("/auth/signin");
   };
 
   const userName = user?.user_metadata?.full_name || user?.email || "User";
   const firstLetter = userName.charAt(0).toUpperCase();
 
-  return user ? (
-    <div className="flex items-center gap-4">
+  return (
+    user && (
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Avatar className="hover:cursor-pointer">
@@ -62,13 +60,6 @@ export default async function AuthButton() {
           )}
         </DropdownMenuContent>
       </DropdownMenu>
-    </div>
-  ) : (
-    <Link
-      href="/login"
-      className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
-    >
-      Login
-    </Link>
+    )
   );
 }
