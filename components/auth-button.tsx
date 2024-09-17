d06741d462fb0a28c3afde51e4a2d3a7c4c8411a
Alejandro Pérez Durán <aperez@datamart.cl>
2024-09-16 20:42:53 -0400

    Refactor AuthButton component to add dropdown menu for user actions

--- components/auth-button.tsx
+++ components/auth-button.tsx
@@ -1,6 +1,16 @@
 import { createClient } from "@/utils/supabase/server";
+import { LogOut, Settings, User } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -17,19 +27,46 @@ export default async function AuthButton() {
     return redirect("/login");
   };
 
+  const userName = user?.user_metadata?.full_name || user?.email || "User";
+  const firstLetter = userName.charAt(0).toUpperCase();
+
   return user ? (
     <div className="flex items-center gap-4">
-      Hey, {user.email}!
-      <form action={signOut}>
-        <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
-          Logout
-        </button>
-      </form>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className="hover:cursor-pointer">
+            {/* <AvatarImage src="https://github.com/shadcn.png" alt="user" /> */}
+            <AvatarFallback>{firstLetter}</AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>{userName}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            <User className="mr-2 h-4 w-4" />
+            <span>Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+          </DropdownMenuItem>
+          {user && (
+            <form action={signOut}>
+              <DropdownMenuItem>
+                <button type="submit" className="flex w-full items-center">
+                  <LogOut className="mr-2 h-4 w-4" />
+                  Logout
+                </button>
+              </DropdownMenuItem>
+            </form>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   ) : (
     <Link
       href="/login"
-      className="py-2 px-3 flex rounded-md no-underline bg-btn-background hover:bg-btn-background-hover"
+      className="bg-btn-background hover:bg-btn-background-hover flex rounded-md px-3 py-2 no-underline"
     >
       Login
     </Link>
